b70b587a24eccf539dfd62c1b9306840b124f7fc
Floris Robbemont <florisrobbemont@hotmail.nl>
2012-07-22 18:14:13 +0200

    Added Country utils

---
+++ src/KeyHub.Common/Utils/Globalization/Countries.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyHub.Common.Utils.Globalization
+{
+    /// <summary>
+    /// Provides access to common country related tasks
+    /// </summary>
+    public static class Countries
+    {
+        /// <summary>
+        /// Gets a list of available countries
+        /// </summary>
+        /// <returns>
+        /// A dictionary with countries using the two-letter country indentifier as key,
+        /// and the English name as value
+        /// </returns>
+        public Dictionary<string, string> GetAllCountries()
+        {
+            Dictionary<string, string> countryDictionary = new Dictionary<string, string>();
+
+            // Get all specific cultures from the Framework
+            foreach (CultureInfo cultureInfo in CultureInfo.GetCultures(System.Globalization.CultureTypes.SpecificCultures))
+            {
+                // Add the region information from this culture to the country list, if not already present
+                RegionInfo regionInfo = new RegionInfo(cultureInfo.Name);
+                if (!countryDictionary.ContainsKey(regionInfo.EnglishName))
+                {
+                    countryDictionary.Add(regionInfo.EnglishName, regionInfo.TwoLetterISORegionName.ToLower());
+                }
+            }
+
+            return countryDictionary;
+        }
+    }
+}
