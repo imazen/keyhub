407b640304d781a3051570616bde82ef3f077aa8
Steven Somer <steven@lucrasoft.nl>
2012-08-08 17:42:46 +0200

    Added mockup for User and Rights dataextension

---
+++ src/KeyHub.DataExtensions/RightsObjectExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyHub.Model;
+
+namespace KeyHub
+{
+    public static class RightsObjectExtensions
+    {
+        public static IQueryable<TEntity> FilterByUser<TEntity>(this IQueryable<TEntity> query, User user) where TEntity : RightObject
+        {
+            // TODO: Filter on rights table
+            return query;
+        }
+    }
+}
---
+++ src/KeyHub.DataExtensions/UserExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyHub.Model;
+
+namespace KeyHub
+{
+    public static class UserExtensions
+    {
+        public static User GetByUserName(this DbSet<User> userSet, string userName)
+        {
+            return userSet.FirstOrDefault(x => x.UserName == userName);
+        }
+    }
+}
