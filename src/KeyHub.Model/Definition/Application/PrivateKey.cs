638f021dd1829f68c9e225d319cbd6332151e35e
Steven Somer <steven@lucrasoft.nl>
2012-08-14 14:46:00 +0200

    Added Controller, ViewModel and Views for PrivateKey

--- src/KeyHub.Model/Definition/Application/PrivateKey.cs
+++ src/KeyHub.Model/Definition/Application/PrivateKey.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +12,7 @@ namespace KeyHub.Model
     /// <summary>
     /// Private key entity for encryption/decryption purposes of license keys.
     /// </summary>
-    public class PrivateKey
+    public partial class PrivateKey
     {
         /// <summary>
         /// Indentifier for the PrivateKey entity.
---
+++ src/KeyHub.Web/ViewModels/PrivateKey/PrivateKeyCreateViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeyHub.Web.ViewModels.PrivateKey
+{
+    /// <summary>
+    /// Viewmodel for creation of a PrivateKey
+    /// </summary>
+    public class PrivateKeyCreateViewModel : BaseViewModel<Model.PrivateKey>
+    {
+        public PrivateKeyCreateViewModel() : base() { }
+
+        /// <summary>
+        /// Construct the viewmodel
+        /// </summary>
+        /// <param name="vendor">Vendor of this PrivateKey</param>
+        public PrivateKeyCreateViewModel(Model.Vendor vendor)
+        {
+            Model.PrivateKey privateKey = new Model.PrivateKey();
+            privateKey.SetKeyBytes();
+
+            PrivateKey = new PrivateKeyViewModel(privateKey);
+            PrivateKey.VendorId = vendor.ObjectId;
+        }
+        
+        /// <summary>
+        /// Edited privateKey
+        /// </summary>
+        public PrivateKeyViewModel PrivateKey { get; set; }
+
+        /// <summary>
+        /// Convert back to PrivateKey instance
+        /// </summary>
+        /// <returns>New privateKey containing viewmodel data </returns>
+        public override Model.PrivateKey ToEntity(Model.PrivateKey original)
+        {
+            return PrivateKey.ToEntity(null);
+        }
+    }
+}
