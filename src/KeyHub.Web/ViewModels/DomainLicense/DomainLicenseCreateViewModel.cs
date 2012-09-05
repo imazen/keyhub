ff49510a469b7e4067f83e6fad3ac865799bf7ce
Steven Somer <steven@lucrasoft.nl>
2012-09-05 13:38:51 +0200

    Added DomainLicense Create and Edit

---
+++ src/KeyHub.Web/ViewModels/DomainLicense/DomainLicenseCreateViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using KeyHub.Data;
+
+namespace KeyHub.Web.ViewModels.DomainLicense
+{
+    /// <summary>
+    /// Viewmodel for creation of a DomainLicense
+    /// </summary>
+    public class DomainLicenseCreateViewModel : BaseViewModel<Model.DomainLicense>
+    {
+        public DomainLicenseCreateViewModel() : base() { }
+
+        /// <summary>
+        /// Construct the viewmodel
+        /// </summary>
+        /// <param name="licenses">Licenses for selectionlist</param>
+        public DomainLicenseCreateViewModel(List<Model.License> licenses)
+        {
+            DomainLicense = new DomainLicenseViewModel(new Model.DomainLicense());
+            LicenseList = licenses.ToSelectList(v => v.ObjectId, v => v.Sku.SkuCode);
+        }
+
+        /// <summary>
+        /// Edited DomainLicense
+        /// </summary>
+        public DomainLicenseViewModel DomainLicense { get; set; }
+
+        /// <summary>
+        /// List of licenses to select
+        /// </summary>
+        public SelectList LicenseList { get; set; }
+
+        /// <summary>
+        /// Convert back to DomainLicense instance
+        /// </summary>
+        /// <returns>New DomainLicense containing viewmodel data </returns>
+        public override Model.DomainLicense ToEntity(Model.DomainLicense original)
+        {
+            return DomainLicense.ToEntity(null);
+        }
+    }
+}
