2b2f051db20b4c754ef73d8cef84277282e96abc
Floris Robbemont <florisrobbemont@hotmail.nl>
2013-02-19 19:37:13 +0100

    Extra merge changes

---
+++ src/KeyHub.Web/Installers/UnitOfWorkInstaller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Castle.MicroKernel.Registration;
+using KeyHub.Data.ApplicationIssues;
+
+namespace KeyHub.Web.Installers
+{
+    public class UnitOfWorkInstaller : IWindsorInstaller
+    {
+        public void Install(Castle.Windsor.IWindsorContainer container, Castle.MicroKernel.SubSystems.Configuration.IConfigurationStore store)
+        {
+            container.Register(
+                Component.For<IApplicationIssueUnitOfWork>()
+                         .ImplementedBy<ApplicationIssueUnitOfWork>()
+                         .LifestyleTransient());
+        }
+    }
+}
