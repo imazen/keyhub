25d9ed107735fb57ee7f57a1c15778e932f4403a
Floris Robbemont <florisrobbemont@hotmail.nl>
2013-02-22 00:21:09 +0100

    Added Elmah logging module

--- src/KeyHub.Web/Composition/WindsorControllerFactory.cs
+++ src/KeyHub.Web/Composition/WindsorControllerFactory.cs
@@ -22,10 +22,12 @@ namespace KeyHub.Web.Composition
 
         protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType)
         {
-            if (controllerType == null)
+            if (controllerType == null || !kernel.HasComponent(controllerType))
             {
+                return base.GetControllerInstance(requestContext, controllerType);
                 throw new HttpException(404, string.Format("The controller for path '{0}' could not be found.", requestContext.HttpContext.Request.Path));
             }
+
             return (IController)kernel.Resolve(controllerType);
         }
     }
