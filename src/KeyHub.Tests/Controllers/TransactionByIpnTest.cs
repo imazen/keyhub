cd4f40bde0e9b02fe6410a0e2cc2e9de56dd03ef
Nathanael Jones <nathanael.jones@gmail.com>
2013-02-22 15:20:03 +0100

    Replaced old IpnTest data with new SKU and a test IPN with my own info

--- src/KeyHub.Tests/Controllers/TransactionByIpnTest.cs
+++ src/KeyHub.Tests/Controllers/TransactionByIpnTest.cs
@@ -17,8 +17,8 @@ namespace KeyHub.Tests.Controllers
     public class TransactionByIpnTest
     {
         private static readonly Guid VendorGuid = new Guid("ED20D311-3F63-436C-8D91-6BADEDF8D2BC");
-        private const string SkuName = "929356||R3Bundle1Ent";
-        private const string Request = "residence_country=GB&payer_business_name=Business+LLC&first_name=Michael&last_name=Crichton&payer_email=crichton%40ndj7.com&payer_phone=%2B44+1624+334410&payer_street=Douglas+Central+Telephone+Exchange%0ADalton+Street&payer_city=Douglas&payer_state=Isle+of+Man&payer_zip=IM1+3PQ&payer_country_code=GB&address_name=+&address_business_name=&address_phone=&address_street=&address_city=&address_state=&address_zip=&address_country_code=US&address_country=US&payment_date=04%3A31%3A49+Dec+04%2C+2012+MST&custom=&mc_currency=USD&business=billing%40imazen.io&mc_gross=249&mc_shipping=0&tax=0&txn_type=ppdirect&payment_type=Instant&invoice=wc8dzhx7k5j9v56yfo058bw0sgoocw0gwck8ow&buyer_ip=195.10.113.83&card_last_four=9271&card_type=MasterCard&mailing_list_status=true&charset=utf-8&item_name1=Resizer+3&item_number1=929356%7C%7CR3Bundle1Ent&mc_gross_1=249&quantity1=1&num_cart_items=1&txn_id=7EG58066DH656324X&payment_status=Completed&pending_reason=&item_name=Resizer+3&item_number=929356%7C%7CR3Bundle1Ent&quantity=1&option_name1=&option_selection1=&option_name2=&option_selection2=&option_name3=&option_selection3=&contact_phone=%2B44+1624+634410&handshake=ff35a320762dcec799d9c0bb9831577c&discount_codes=&from_name=Imazen&from_email=billing%40imazen.io&mailing_list_status=true&client_shipping_method_id=0&item_cart_position=1&sku=R3Bundle1Ent&expiry_hours=0&max_downloads=9&ej_txn_id=16356000";
+        private const string SkuName = "1197635||R3Performance";
+        private const string Request = "residence_country=US&payer_business_name=NDJ+Software&first_name=Nathanael&last_name=Jones&payer_email=n%40ndj7.com&payer_phone=5025091177&payer_street=422+Knobloch+Ave.&payer_city=Jeffersonville&payer_state=IN&payer_zip=47130&payer_country_code=US&address_name=+&address_business_name=&address_phone=&address_street=&address_city=&address_state=&address_zip=&address_country_code=US&address_country=US&payment_date=07%3A15%3A08+Feb+22%2C+2013+MST&custom=&mc_currency=USD&business=billing%40imazen.io&mc_gross=2.49&mc_shipping=0&tax=0&txn_type=ppdirect&payment_type=Instant&invoice=wc8e22ji5d7ouakpyehkyi9cs4kckscswgwgg4&buyer_ip=79.24.248.176&card_last_four=1234&card_type=MasterCard&mailing_list_status=true&charset=utf-8&item_name1=Resizer+3.X&item_number1=1197635%7C%7CR3Performance&mc_gross_1=2.49&quantity1=1&num_cart_items=1&txn_id=00T198685W3838305&payment_status=Completed&pending_reason=&item_name=Resizer+3.X&item_number=1197635%7C%7CR3Performance&quantity=1&option_name1=&option_selection1=&option_name2=&option_selection2=&option_name3=&option_selection3=&contact_phone=5025091177&handshake=ff35a320762dcec799d9c0bb9831577c&discount_codes=For+Cart+Item+Total%3A+T315125125&from_name=Imazen&from_email=billing%40imazen.io&mailing_list_status=true&client_shipping_method_id=0&item_cart_position=1&sku=R3Performance&expiry_hours=0&max_downloads=9&ej_txn_id=17534028";
 
         private TestContext testContextInstance;
         private TransactionByIpnController controller;
@@ -80,7 +80,8 @@ namespace KeyHub.Tests.Controllers
         {
             controller.PostTransactionByIpn(
                 VendorGuid.ToString(), 
-                new System.Net.Http.Formatting.FormDataCollection(Request));
+                new System.Net.Http.
+                    Formatting.FormDataCollection(Request));
         }
     }
 }
