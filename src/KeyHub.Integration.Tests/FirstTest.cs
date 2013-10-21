a963eadd7865bbd17db2ad6edc7132102fafc5a3
fschwiet <fschwiet@gmail.com>
2013-10-21 14:03:57 -0700

    have a test that can reset database and run KeyHub.Web

---
+++ src/KeyHub.Integration.Tests/FirstTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace KeyHub.Integration.Tests
+{
+    public class FirstTest
+    {
+        [Fact]
+        public void CanRunServer()
+        {
+            ResetLocalhostDatabase.Run();
+
+            using (var site = new IISExpressDriver())
+            {
+                var path = Path.GetFullPath("../../../KeyHub.Web");
+
+                if (!Directory.Exists(path))
+                    throw new Exception("Could not find KeyHub.Web");
+
+                site.Start("KeyHub.Web", "../../applicationhost.config");
+
+                using (var client = new WebClient())
+                {
+                    Console.WriteLine(site.Url);
+                    Console.WriteLine(client.DownloadString(site.Url));
+                }
+
+            }
+        }
+    }
+}
---
+++ src/KeyHub.Integration.Tests/ResetLocalhostDatabase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyHub.Integration.Tests
+{
+    public class ResetLocalhostDatabase : ProcessDriver
+    {
+        public static void Run()
+        {
+            using (var stopProcess = new ResetLocalhostDatabase())
+            {
+                stopProcess.StartProcess("SQLLocalDB.exe", "stop v11.0");
+
+                stopProcess.WaitForConsoleOutputMatching(@"(stopped\.)|(does not exist)");
+            }
+
+            using (var deleteProcess = new ResetLocalhostDatabase())
+            {
+                deleteProcess.StartProcess("SQLLocalDB.exe", "delete v11.0");
+
+                deleteProcess.WaitForConsoleOutputMatching(@"(deleted\.)|(does not exist)");
+            }
+        }
+    }
+}
