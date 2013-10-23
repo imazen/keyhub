a79ebec02633cdaca9f7943224b9aa8e698bc0f2
fschwiet <fschwiet@gmail.com>
2013-10-23 16:01:46 -0700

    starting LicenseValidatorTests

---
+++ src/KeyHub.Integration.Tests/CheckedDataContextFactory.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using KeyHub.Data;
+
+namespace KeyHub.Integration.Tests
+{
+    public class CheckedDataContextFactory : IDataContextFactory, IDisposable
+    {
+        private int instanceCount;
+        private int releaseCount;
+        private IIdentity identity;
+        
+        public CheckedDataContextFactory(IIdentity identity)
+        {
+            this.instanceCount = 0;
+            this.releaseCount = 0;
+            this.identity = identity;
+        }
+
+        public IDataContext Create()
+        {
+            instanceCount++;
+            return new DataContext();
+        }
+
+        public IDataContextByUser CreateByUser()
+        {
+            instanceCount++;
+            return new DataContextByUser(identity);
+        }
+
+        public IDataContextByTransaction CreateByTransaction(Guid transactionId)
+        {
+            instanceCount++;
+            return new DataContextByTransaction(identity, transactionId);
+        }
+
+        public void Release(IDataContext dataContext)
+        {
+            releaseCount++;
+            dataContext.Dispose();
+        }
+
+        public void Dispose()
+        {
+            /*  I think we have a bug- the code is called .Dispose() on the contexts, but Windsor expects factory.Release(context) to be used.
+            if (releaseCount != instanceCount)
+                throw new Exception(String.Format("CheckedDataContextFactory detected a leak- {0} allocations had {1} releases. (the values should match)", instanceCount, releaseCount));
+             */
+        }
+    }
+}
---
+++ src/KeyHub.Integration.Tests/LicenseValidatorTests.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Text;
+using System.Threading.Tasks;
+using KeyHub.BusinessLogic.BusinessRules;
+using KeyHub.BusinessLogic.LicenseValidation;
+using KeyHub.Core.Logging;
+using KeyHub.Data;
+using KeyHub.Data.ApplicationIssues;
+using KeyHub.Integration.Tests.TestSetup;
+using KeyHub.Model;
+using Moq;
+using Xunit;
+
+namespace KeyHub.Integration.Tests
+{
+    public class LicenseValidatorTests
+    {
+        [Fact]
+        [CleanDatabase]
+        public void CanValidateLicense()
+        {
+            var appKey = Guid.NewGuid();
+            var featureGuid = Guid.NewGuid();
+            var domain = "foobar.com";
+
+            using (var dataContext = new DataContext())
+            {
+                var customerApp = new CustomerApp()
+                {
+                    ApplicationName = "test CustomerApp"
+                };
+
+                dataContext.CustomerApps.Add(customerApp);
+                dataContext.SaveChanges();
+
+                Console.WriteLine("id" + customerApp.CustomerAppId);
+
+                dataContext.CustomerAppKeys.Add(new CustomerAppKey()
+                {
+                    CustomerAppId = customerApp.CustomerAppId,
+                    AppKey = appKey
+                });
+
+                dataContext.SaveChanges();
+            }
+
+            var identity = new Mock<IIdentity>(MockBehavior.Loose);
+            var loggingService = new Mock<ILoggingService>(MockBehavior.Loose);
+            var applicationIssuesUnitOfWork = new Mock<IApplicationIssueUnitOfWork>(MockBehavior.Loose);
+
+            using (var dataContextFactory = new CheckedDataContextFactory(identity.Object))
+            {
+                var validator = new LicenseValidator(dataContextFactory, loggingService.Object,
+                    applicationIssuesUnitOfWork.Object);
+
+                var result = validator.Validate(appKey, new[] {new DomainValidation(domain, featureGuid)}).Single();
+
+                Assert.Equal(result.DomainName, "foorbar.com");
+                Assert.Contains(featureGuid, result.Features);
+            }
+        }
+    }
+}
