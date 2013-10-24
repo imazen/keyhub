340f624f56b0f9c95be8bc5d2c7d4bd185b91b32
fschwiet <fschwiet@gmail.com>
2013-10-24 16:26:30 -0700

    adding notes on how to debug when using KeyHubWebDriver

--- src/KeyHub.Integration.Tests/TestSetup/KeyHubWebDriver.cs
+++ src/KeyHub.Integration.Tests/TestSetup/KeyHubWebDriver.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace KeyHub.Integration.Tests.TestSetup
 {
+    //  When using KeyHubWebDriver, it is more difficult to debug the server code since it
+    //  runs in a separate process without the debugger.  To debug the server code in this case:
+    //
+    //  1.  Call Debugger.Launch() from the server-side code you want to debug (Debugger.Break does not work)
+    //  2.  Run the test without the debugger
+    //
+    //  When the test hits the server-side code that calls Debugger.Launch(), you should be propmpted to
+    //  choose a debugger for the process.
+
     class KeyHubWebDriver : IISExpressDriver
     {
         public KeyHubWebDriver()
