22538e9fd10ae5b9103f066594d9a97b329d5fe1
Frank <fschwiet@gmail.com>
2013-10-29 11:58:03 -0700

    not sure why rm isn't aliased on this machine..

--- scripts/cleanLocalhostDatabase.bat
+++ scripts/cleanLocalhostDatabase.bat
@@ -1,4 +1,4 @@
 sqllocaldb stop v11.0
 sqllocaldb delete v11.0
-rm .\src\KeyHub.web\App_Data\keyhub.mdf
-rm .\src\KeyHub.web\App_Data\KeyHub_log.ldf
+erase .\src\KeyHub.web\App_Data\keyhub.mdf
+erase .\src\KeyHub.web\App_Data\KeyHub_log.ldf
