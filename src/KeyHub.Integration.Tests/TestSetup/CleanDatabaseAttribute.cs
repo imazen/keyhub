fb180db963e055b445a87a9d8875c8eb613b47d1
Frank <fschwiet@gmail.com>
2013-10-30 11:16:28 -0700

    fix database cleanup for tests (manually deleting the database was leaving bad connections in the connection pool)

--- src/KeyHub.Integration.Tests/TestSetup/CleanDatabaseAttribute.cs
+++ src/KeyHub.Integration.Tests/TestSetup/CleanDatabaseAttribute.cs
@@ -1,51 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity.Infrastructure;
+using System.Data.EntityClient;
+using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using KeyHub.Data;
 using Xunit;
 
 namespace KeyHub.Integration.Tests.TestSetup
 {
     public class CleanDatabaseAttribute : BeforeAfterTestAttribute
     {
-        private string GetDatabaseDirectory()
-        {
-            //return Path.GetFullPath("../../../KeyHub.Web/App_Data");
-            return "c:\\temp";
-        }
-
         public override void Before(MethodInfo methodUnderTest)
         {
-            using (var stopProcess = new PlainProcessDriver())
+            using (var context = new DataContext())
             {
-                stopProcess.StartProcess("SQLLocalDB.exe", "stop v11.0");
-
-                stopProcess.WaitForConsoleOutputMatching(@"(stopped\.)|(does not exist)");
-            }
-
-            using (var deleteProcess = new PlainProcessDriver())
-            {
-                deleteProcess.StartProcess("SQLLocalDB.exe", "delete v11.0");
-
-                deleteProcess.WaitForConsoleOutputMatching(@"(deleted\.)|(does not exist)");
-            }
-
-            var dataDirectory = GetDatabaseDirectory();
-            if (!Directory.Exists(dataDirectory))
-                throw new Exception("Could not find data directory " + dataDirectory);
-
-            foreach (var file in new[]
-            {
-                "KeyHub.mdf",
-                "KeyHub_log.ldf"
-            })
-            {
-                var fullFilepath = Path.Combine(dataDirectory, file);
-                if (File.Exists(fullFilepath))
-                    File.Delete(fullFilepath);
+                context.Database.Delete();
             }
         }
     }
