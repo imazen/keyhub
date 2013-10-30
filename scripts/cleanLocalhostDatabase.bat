5cb10b098e621c495e210c357b96c82ecfaf3b26
Frank <fschwiet@gmail.com>
2013-10-30 15:20:35 -0700

    updating local testing script to clear database (weird I thought this had been updated already)

--- scripts/cleanLocalhostDatabase.bat
+++ scripts/cleanLocalhostDatabase.bat
@@ -1,4 +1,4 @@
 sqllocaldb stop v11.0
 sqllocaldb delete v11.0
-erase .\src\KeyHub.web\App_Data\keyhub.mdf
-erase .\src\KeyHub.web\App_Data\KeyHub_log.ldf
+erase c:\temp\keyhub.mdf
+erase c:\temp\KeyHub_log.ldf
