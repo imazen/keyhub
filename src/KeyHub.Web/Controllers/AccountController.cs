ca69ce6f7d9bda58304c0fc4b9fa1b2b01909d3d
Frank <fschwiet@gmail.com>
2013-11-12 13:27:48 -0800

    AccountController using anti-forgery token for all posts

--- src/KeyHub.Web/Controllers/AccountController.cs
+++ src/KeyHub.Web/Controllers/AccountController.cs
@@ -128,7 +128,7 @@ namespace KeyHub.Web.Controllers
         /// </summary>
         /// <param name="viewModel">Edited UserViewModel</param>
         /// <returns>Redirectaction to index if successful</returns>
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Edit(UserEditViewModel viewModel)
         {
             if (ModelState.IsValid)
@@ -172,7 +172,7 @@ namespace KeyHub.Web.Controllers
         /// <param name="returnUrl"></param>
         /// <returns></returns>
         [AllowAnonymous]
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult Login(LoginViewModel model, string returnUrl)
         {
             if (ModelState.IsValid)
@@ -233,8 +233,7 @@ namespace KeyHub.Web.Controllers
         /// <param name="model">RegisterViewModel for new user</param>
         /// <param name="returnUrl">Url to redirect to after successfull registration</param>
         /// <returns>Redirect to Index, or ReturnUrl if specified</returns>
-        [AllowAnonymous]
-        [HttpPost]
+        [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
         public ActionResult Register(RegisterViewModel model, string returnUrl)
         {
             if (ModelState.IsValid)
@@ -296,7 +295,7 @@ namespace KeyHub.Web.Controllers
         /// </summary>
         /// <param name="model">Changed password model</param>
         /// <returns></returns>
-        [HttpPost]
+        [HttpPost, ValidateAntiForgeryToken]
         public ActionResult ChangePassword(ChangePasswordViewModel model)
         {
             if (ModelState.IsValid)
@@ -488,46 +487,6 @@ namespace KeyHub.Web.Controllers
             public override void ExecuteResult(ControllerContext context)
             {
                 OAuthWebSecurity.RequestAuthentication(Provider, ReturnUrl);
-            }
-        }
-        #endregion
-
-        #region Status Codes
-        private static string ErrorCodeToString(MembershipCreateStatus createStatus)
-        {
-            // See http://go.microsoft.com/fwlink/?LinkID=177550 for
-            // a full list of status codes.
-            switch (createStatus)
-            {
-                case MembershipCreateStatus.DuplicateUserName:
-                    return "User name already exists. Please enter a different user name.";
-
-                case MembershipCreateStatus.DuplicateEmail:
-                    return "A user name for that e-mail address already exists. Please enter a different e-mail address.";
-
-                case MembershipCreateStatus.InvalidPassword:
-                    return "The password provided is invalid. Please enter a valid password value.";
-
-                case MembershipCreateStatus.InvalidEmail:
-                    return "The e-mail address provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidAnswer:
-                    return "The password retrieval answer provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidQuestion:
-                    return "The password retrieval question provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.InvalidUserName:
-                    return "The user name provided is invalid. Please check the value and try again.";
-
-                case MembershipCreateStatus.ProviderError:
-                    return "The authentication provider returned an error. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                case MembershipCreateStatus.UserRejected:
-                    return "The user creation request has been canceled. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
-
-                default:
-                    return "An unknown error occurred. Please verify your entry and try again. If the problem persists, please contact your system administrator.";
             }
         }
         #endregion
