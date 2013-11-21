586712912a662666af48160e09ad040a73785c53
Frank <fschwiet@gmail.com>
2013-11-21 10:56:25 -0800

    Add a route for /Shared/Error

---
+++ src/KeyHub.Web/Controllers/SharedController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KeyHub.Web.Controllers
+{
+    public class SharedController : Controller
+    {
+        public ActionResult Error()
+        {
+            return View();
+        }
+    }
+}
